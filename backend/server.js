89d1b131fcd5142c58e55a66f62e70a6e2fe6642
Nandakumar MV <nandu2255n@gmail.com>
2023-12-15 21:35:19 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://nandakumarmv.co", credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -34,7 +34,7 @@ import { Server } from "socket.io";
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://nandakumarmv.co",
   },
 });
 io.on("connection", (socket) => {
