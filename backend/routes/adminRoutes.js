eca408f696859356d65569748fdc5b9436eb6d33
Nandakumar MV <nandu2255n@gmail.com>
2023-12-16 10:32:39 +0530

    domain error solved

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -30,7 +30,7 @@ router.post("/register", registerAdmin);
 router.post("/logout", logoutAdmin);
 router.get("/users", protect("admin"), userList);
 router.get("/tutors", protect("admin"), tutorList);
-router.get("/domains", protect("admin"), getDomains);
+router.get("/domains", getDomains);
 router.post("/block-user", blockUser);
 router.post("/unblock-user", unblockUser);
 router.post("/unblock-tutor", tutorunblockUser);
